d53add704988d180204151a1132c64b2b02008ab
avinashnsa <avinashnsa89@gmail.com>
2021-09-02 13:15:25 +0530

    'New'

---
+++ cypress/integration/Myprac.js
@@ -0,0 +1,9 @@
+/// <reference types='cypress'/>
+describe('My First Suite',function()
+{
+    it('My First test case',function()
+    {
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+
+    })
+})
